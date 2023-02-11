6664cc38bf30d3675e63175e662e920b4f981a16
“zubairJr” <zubairmehmood199@gmail.com>
2023-02-11 20:37:41 +0500

    side nav added

--- src/app/components/user/sidenav/sidenav.component.ts
+++ src/app/components/user/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +10,49 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  constructor(private _active:ActivatedRoute,private _route:Router){}
+
+
+
+  home():void{
+    
+    if(this._route.url ==='/user-dashboard/quiz'){
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this._route.navigate(['user-dashboard'])
+        }
+      })
+    }
+    else{
+      this._route.navigate(['user-dashboard'])
+    }
+  }
+  logout():void{
+    if(this._route.url ==='/user-dashboard/quiz'){
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          
+          this._route.navigate([''])
+        }
+      })
+    }else{
+      this._route.navigate([''])
+    }
+  }
 }
